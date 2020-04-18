a8db9afcb1faff260ec7b2b5e9738132d0d4bda8
Afdal Ghozali <afdalghoz12@gmail.com>
2020-04-18 05:30:53 -0700

    commit pertama

---
+++ Src/ch03/pembagi.java
@@ -0,0 +1,10 @@
+public class pembagian terkecil {
+	  public static void main (String []args ){
+		  int x = 1o;
+		  int y = 5;
+		  
+		System.out.println("10/5 = 2");
+		System.out.println("nilai 5 adalah pembagi terkecil selain 1 dan 2");
+		System.out.println("nilai = "+y);
+	  }
+}
